000773d610bf438ee2f7a9c5827158c607cc6eb6
Satyam0004 <satyamkmr04@gmail.com>
2026-02-04 19:57:54 +0530

    Config management

--- additional/config-management/configdemo/src/main/java/com/info/configdemo/BuildInfoController.java
+++ additional/config-management/configdemo/src/main/java/com/info/configdemo/BuildInfoController.java
@@ -1,5 +1,6 @@
 package com.info.configdemo;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -7,19 +8,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class BuildInfoController {
 
-    @Value("${build.id}")
-    private String buildId;
+//    @Value("${build.id}")
+//    private String buildId;
+//
+//    @Value("${build.version}")
+//    private String buildVersion;
+//
+//    @Value("${build.name}")
+//    private String buildName;
 
-    @Value("${build.version}")
-    private String buildVersion;
-
-    @Value("${build.name}")
-    private String buildName;
+    private BuildInfo buildInfo;
 
     @GetMapping("/build-info")
     private String getBuildInfo() {
-        return "Build ID: " + buildId + ", Build Version: " + buildVersion + ", Build Name: " + buildName;
+        return "Build ID: " + buildInfo.getId() + ", Build Version: " + buildInfo.getVersion() + ", Build Name: " + buildInfo.getName();
     }
 }

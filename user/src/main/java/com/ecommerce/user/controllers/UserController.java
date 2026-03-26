9e9410e84de88ee56ad48a84b18d46f5728ea3b4
Satyam0004 <satyamkmr04@gmail.com>
2026-03-26 20:03:06 +0530

    Making use of @Slf4j lombok and creating log files ./logs

--- user/src/main/java/com/ecommerce/user/controllers/UserController.java
+++ user/src/main/java/com/ecommerce/user/controllers/UserController.java
@@ -4,6 +4,9 @@ import com.ecommerce.user.dtos.UserRequest;
 import com.ecommerce.user.dtos.UserResponse;
 import com.ecommerce.user.services.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +15,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
+@Slf4j
 public class UserController {
 
     private final UserService userService;
@@ -29,7 +33,11 @@ public class UserController {
 
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> getUsersById(@PathVariable String id) {
-
+        log.info("Request received for user: {}", id);
+        log.trace("this is trace level");
+        log.debug("this is debug level");
+        log.warn("this is warn level");
+        log.error("this is error level");
         return userService.fetchUser(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());

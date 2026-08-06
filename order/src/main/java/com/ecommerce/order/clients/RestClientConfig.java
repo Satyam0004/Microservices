8f5586d1e098618873af3d64abcc5a4ff5bde69f
Satyam0004 <satyamkmr04@gmail.com>
2026-08-06 14:39:04 +0530

    observability in spring boot: logs, matrices, tracing with loki and prometheus

--- order/src/main/java/com/ecommerce/order/clients/RestClientConfig.java
+++ order/src/main/java/com/ecommerce/order/clients/RestClientConfig.java
@@ -1,5 +1,9 @@
 package com.ecommerce.order.clients;
 
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.tracing.Tracer;
+import io.micrometer.tracing.propagation.Propagator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +11,16 @@ import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfig {
+
+    @Autowired(required = false)
+    private ObservationRegistry observationRegistry;
+
+    @Autowired(required = false)
+    private Tracer tracer;
+
+    @Autowired(required = false)
+    private Propagator propagator;
+
     @Bean
     @LoadBalanced
     public RestClient.Builder restClientBuilder() {
--- user/src/main/java/com/ecommerce/user/controllers/UserController.java
+++ user/src/main/java/com/ecommerce/user/controllers/UserController.java
@@ -5,8 +5,6 @@ import com.ecommerce.user.dtos.UserResponse;
 import com.ecommerce.user.services.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 

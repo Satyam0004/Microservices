09f2d4d67f82ef767929e239574363f00fcae61f
Satyam0004 <satyamkmr04@gmail.com>
2026-08-13 22:59:59 +0530

    Adding FallBack Mechanism

--- order/src/main/java/com/ecommerce/order/services/CartService.java
+++ order/src/main/java/com/ecommerce/order/services/CartService.java
@@ -24,7 +24,7 @@ public class CartService {
     private final ProductServiceClient productServiceClient;
     private final UserServiceClient userServiceClient;
 
-    @CircuitBreaker(name = "productService")
+    @CircuitBreaker(name = "productService", fallbackMethod = "addToCartFallBack")
     public boolean addToCart(String userId, CartItemRequest request) {
         ProductResponse productResponse = productServiceClient.getProductDetails(String.valueOf(request.getProductId()));
 
@@ -56,6 +56,12 @@ public class CartService {
         }
 
         return true;
+    }
+
+    public boolean addToCartFallBack(String userId, CartItemRequest request, Exception exception) {
+//        System.out.println("FALLBACK CALLED");
+        exception.printStackTrace();
+        return false;
     }
 
     public boolean deleteItemFromCart(String userId, Long productId) {

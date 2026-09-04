eab15eef0b252c80593fd82c9717d887ba9cbb5d
Satyam0004 <satyamkmr04@gmail.com>
2026-09-04 10:42:41 +0530

    configured retry meachanism for order application

---
+++ order/src/main/java/com/ecommerce/order/controllers/MessageController.java
@@ -0,0 +1,25 @@
+package com.ecommerce.order.controllers;
+
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class MessageController {
+
+    @Value("${app.message}")
+    private String message;
+
+    @GetMapping("/message")
+    @RateLimiter(name = "rateBreaker", fallbackMethod = "getMessageFallBack")
+    public String getMessage() {
+        return message;
+    }
+
+    public String getMessageFallBack(Exception ex) {
+        return "Hello FallBack";
+    }
+}
